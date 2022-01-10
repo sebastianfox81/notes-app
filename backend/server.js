5fc686902c324e950bbb42e296f2b0c48daf7f29
Sebastian <sebastianfox81@gmail.com>
2022-01-09 21:38:26 -0500

    Created connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// CONNECT TO MONGOOSE DATABASE
+const URI = 'mongodb://localhost/notesapp';
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Connection to MongoDB established successfully')
+});
+
+// CONNECT EXPRESS SERVER
 app.listen(5000, () => {
   console.log('Listening on port 5000')
 })
