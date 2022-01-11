62a8c6d456409301ca61cadc75e5d32418308eaa
Sebastian <sebastianfox81@gmail.com>
2022-01-10 23:24:50 -0500

    Rendered user list from the backend

--- client/src/components/CreateUser.js
+++ client/src/components/CreateUser.js
@@ -1,12 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 class CreateUser extends Component {
+  state = {
+    users: [],
+  };
+
+  async componentDidMount() {
+    const res = await axios.get("http://localhost:5000/api/users");
+    this.setState({
+      users: res.data,
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h1>Create User</h1>
+      <div className="row">
+        <div>
+          <ul>
+            {this.state.users.map((user) => {
+              return (
+                <li key={user._id} className="list-group-item list-group-item-action">
+                  {user.username}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-    )
+    );
   }
 }
 
